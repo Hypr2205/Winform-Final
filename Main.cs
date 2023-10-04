80363551f2a314e6900387e175e72d9cfc71dfe2
Astronaut <procango2003@gmail.com>
2023-10-05 01:20:20 +0700

    Tạo ui cho form chính + load và hiển thị thời gian của hôm nay lên

--- Main.cs
+++ Main.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
 
 namespace Final {
     public partial class Main : Form {
@@ -7,7 +9,22 @@ namespace Final {
         }
 
         private void Main_Load(object sender, System.EventArgs e) {
+            LoadTime();
+        }
+
+        private void LoadTime() {
+            CultureInfo culture = new CultureInfo("vi-VN");
+            DateTime time = DateTime.Now;
+            LbTime.Text = culture.DateTimeFormat.GetDayName(time.DayOfWeek) + ", " + time.Day + " " + culture.DateTimeFormat.GetMonthName(time.Month) + " " + time.Year;
+        }
+
+        private void BtnPrevious_Click(object sender, EventArgs e) {
 
         }
+
+        private void BtnNext_Click(object sender, EventArgs e) {
+
+        }
+
     }
 }
