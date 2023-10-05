d71e7b541daa5eba460b8b00f31371e161f09a54
Astronaut <procango2003@gmail.com>
2023-10-05 14:00:44 +0700

    UI reset

--- Main.cs
+++ Main.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Final {
     public partial class Main : Form {
@@ -9,22 +7,10 @@ namespace Final {
         }
 
         private void Main_Load(object sender, System.EventArgs e) {
-            LoadTime();
-        }
-
-        private void LoadTime() {
-            CultureInfo culture = new CultureInfo("vi-VN");
-            DateTime time = DateTime.Now;
-            LbTime.Text = culture.DateTimeFormat.GetDayName(time.DayOfWeek) + ", " + time.Day + " " + culture.DateTimeFormat.GetMonthName(time.Month) + " " + time.Year;
-        }
-
-        private void BtnPrevious_Click(object sender, EventArgs e) {
 
         }
 
-        private void BtnNext_Click(object sender, EventArgs e) {
 
-        }
 
     }
 }
