1f252ffb59f74d5dfb63d18690f36aa014d10c5e
Astronaut <procango2003@gmail.com>
2023-10-11 00:01:35 +0700

    Tạo form buildpc và giỏ hàng cho buildpc

---
+++ BuildPcCart.cs
@@ -0,0 +1,13 @@
+﻿using Final.Model.BuildPCModel;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Final {
+    public partial class BuildPcCart : Form {
+        public static List<Accessory> buildPcCartItems = new List<Accessory>();
+
+        public BuildPcCart() {
+            InitializeComponent();
+        }
+    }
+}
