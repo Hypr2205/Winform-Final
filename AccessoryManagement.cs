f8bdfb9cfa871e6b1e11a997a38fbe5fc7da1a48
gaylenpro <kien20038@gmail.com>
2023-10-11 23:05:57 +0700

    add file AccessoryManagement.cs

--- AccessoryManagement.cs
+++ AccessoryManagement.cs
@@ -1,7 +1,9 @@
 ﻿using Final.Model.BuildPCModel;
+using Final.Model.LaptopModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Windows.Forms;
 
 namespace Final {
@@ -23,7 +25,30 @@ namespace Final {
 
         private void BtnSearch_Click(object sender, EventArgs e) {
             //Tìm kiếm, thoả mãn một trong các ô mã linh kiện, tên, hãng, loại linh kiện
+
+            var context = new BuildPcDBContext();
+            try
+            {
+                
+                var findById = context.Accessory.Where(a => a.AccessoryID == TbAccessoryID.Text);
+                var findByName = context.Accessory.Where(b => b.AccessoryName == TbAccessoryName.Text);
+                var findByCbxAccessoryBrand = context.Accessory.Where(c => c.AccessoryBrand.BrandName == ((BuildPcDBContext)CbxAccessoryBrand.SelectedItem).AccessoryBrand.BrandName);
+                var findByCbxAccessoryCategory = context.Accessory.Where(d => d.AccessoryCategory.CategoryName == ((BuildPcDBContext)CbxAccessoryCategory.SelectedItem).AccessoryCategory.AccessoryCategoryName);
+                FillDataView(findById.Union(findByName).Union(findByCbxAccessoryBrand).Union(findByCbxAccessoryCategory).ToList());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             //Có thể kết hợp nhiều điều kiện
+
+            if (TbAccessoryID.Text != "" || TbAccessoryName.Text != "")
+            {
+                FillDataView(context.Accessory.Where(l => l.AccessoryID.Contains(TbAccessoryID.Text) || l.AccessoryName.Contains(TbAccessoryName.Text)).ToList());
+            }
+           
+
+
         }
 
         private void BtnEdit_Click(object sender, EventArgs e) {
@@ -94,7 +119,36 @@ namespace Final {
 
         private void BtnDelete_Click(object sender, EventArgs e) {
             //Xoá một linh kiện kiểm tra xem nếu số lương = 0 thì ko cho xoá
-            //Khi xoá chỉ thay đổi số lượng, không xoá trong db
+            try
+            {
+                BuildPcDBContext context = new BuildPcDBContext();
+                var find = context.Accessory.FirstOrDefault(a => a.AccessoryID == TbAccessoryID.Text);
+                //Nếu không tồn tại
+                if (find.Quantity == 0)
+                {
+                    MessageBox.Show("Số lượng xoá ngoài phạm vi có thể xoá !","Lỗi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return;
+                }
+                //Khi xoá chỉ thay đổi số lượng, không xoá trong db
+                DialogResult dialog = MessageBox.Show("Bạn có muốn xoá ?", "YES/NO", MessageBoxButtons.YesNo);
+                if (int.TryParse(TbQuantity.Text, out int quantity) && quantity > 0 && quantity <= find.Quantity)
+                {
+                    if (dialog == DialogResult.Yes)
+                    {
+                        find.Quantity -= quantity;
+                        context.SaveChanges();
+                        FillDataView(context.Accessory.ToList());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Số lượng xoá ngoài phạm vi có thể xoá !","Lỗi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void FillCategoryBox(IReadOnlyCollection<AccessoryCategory> categories) {
@@ -124,5 +178,7 @@ namespace Final {
                 DgvAccessories.Rows[index].Cells[6].Value = accessory.Quantity.ToString();
             }
         }
+
+        
     }
 }
