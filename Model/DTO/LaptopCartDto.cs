92f3f485d251a049f80cad4138e2577943c5ba16
Tanfuu Lu <lutanfuu@gmail.com>
2023-10-15 18:08:04 +0700

    add value for LaptopCartDto

--- Model/DTO/LaptopCartDto.cs
+++ Model/DTO/LaptopCartDto.cs
@@ -1,4 +1,19 @@
-﻿namespace Final.Model.DTO {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final.Model.DTO {
     public class LaptopCartDto {
-    }
+            public static int counter = 0;
+            public int OrderID { get; set; }
+            public string LaptopID { get; set; }
+            public string LaptopName { get; set; }
+            public decimal SellPrice { get; set; }
+            public int Sale { get; set; }
+            public int CategoryID { get; set; }
+            public int Quantity { get; set; }
+            public int BuyPrice { get; set; }   
+            public int BuyQuantity { get; set; }   
+            public LaptopCartDto() {
+                  OrderID += 1;
+            }
+      }
 }
